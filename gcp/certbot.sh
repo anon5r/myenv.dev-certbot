97ce6db2f10134d5260f5c4018bfbadc44ca1d99
anon <anon@anoncom.net>
2020-02-16 18:14:00 +0900

    Update entrypoint

--- gcp/certbot.sh
+++ gcp/certbot.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 docker run -it --rm \
+    --device /dev/fuse --privileged \
     --dns=8.8.8.8 \
-    gcr.io/myenv-dev/certbot:latest certonly \
-    -m "local@myenv.dev" \
-    -d 'local.myenv.dev' \
-    -d '*.local.myenv.dev' \
-    --dns-cloudflare-propagation-seconds 10
+    --name certbot \
+    gcr.io/myenv-dev/certbot:latest \
+    #  certonly \
+    #  -m "local@myenv.dev" \
+    # -d 'local.myenv.dev' \
+    # -d '*.local.myenv.dev' \
+    # --dns-cloudflare-propagation-seconds 10
--- gcp/initialize.sh
+++ gcp/initialize.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 DOMAIN_NAME=${DOMAIN_NAME:-local.myenv.dev}
+GCS_BUCKET_NAME=${GCS_BUCKET_NAME:-myenvdev-certificates}
 mkdir -p /mnt/gcs/$GCS_BUCKET_NAME
-if [ ! -e /etc/letsencrypt/archives ] ; then
-    mkdir -p /etc/letsencrypt/archives
-fi
-if [ ! -e /etc/letsencrypt/archives/$DOMAIN_NAME ] ; then
-    ln -sfn /mnt/gcs/$GCS_BUCKET_NAME /etc/letsencrypt/archives/$DOMAIN_NAME
-fi
 
-/usr/sbin/gcsfuse $GCS_BUCKET_NAME /etc/letsencrypt/archive/$DOMAIN_NAME $GOOGLE_APPLICATION_CREDENTIALS
+# if [ ! -e /etc/letsencrypt/archive ] ; then
+#     mkdir -p /etc/letsencrypt/archive
+# fi
+# if [ ! -e /etc/letsencrypt/archives/$DOMAIN_NAME ] ; then
+#     ln -sfn /mnt/gcs/$GCS_BUCKET_NAME /etc/letsencrypt/archive/$DOMAIN_NAME
+# fi
+
+#GOOGLE_APPLICATION_CREDENTIALS=/opt/etc/gcs.json /usr/sbin/gcsfuse $GCS_BUCKET_NAME /mnt/gcs/$GCS_BUCKET_NAME
