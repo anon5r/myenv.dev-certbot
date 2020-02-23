ecccf5dcb37a26d230ae6987d9fd1108bd055215
anon <anon@anoncom.net>
2020-02-23 15:18:04 +0900

    Docker run with GCP

--- gcp/certbot.sh
+++ gcp/certbot.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
 docker run -it --rm \
-    --device /dev/fuse --privileged \
+    --privileged \
+    --cap-add SYS_ADMIN --device /dev/fuse \
     --dns=8.8.8.8 \
     --name certbot \
     gcr.io/myenv-dev/certbot:latest \
-    #  certonly \
-    #  -m "local@myenv.dev" \
-    # -d 'local.myenv.dev' \
-    # -d '*.local.myenv.dev' \
-    # --dns-cloudflare-propagation-seconds 10
+    debug
--- gcp/initialize.sh
+++ gcp/initialize.sh
@@ -1,13 +1,45 @@
 #!/bin/sh
 DOMAIN_NAME=${DOMAIN_NAME:-local.myenv.dev}
 GCS_BUCKET_NAME=${GCS_BUCKET_NAME:-myenvdev-certificates}
-mkdir -p /mnt/gcs/$GCS_BUCKET_NAME
+GCSMP=/mnt/gcs/$GCS_BUCKET_NAME
+LE_DIR=/etc/letsencrypt
+echo "Start Initialize"
+mkdir -p $GCSMP
 
 # if [ ! -e /etc/letsencrypt/archive ] ; then
 #     mkdir -p /etc/letsencrypt/archive
 # fi
-# if [ ! -e /etc/letsencrypt/archives/$DOMAIN_NAME ] ; then
+# if [ ! -e /etc/letsencrypt/archive/$DOMAIN_NAME ] ; then
 #     ln -sfn /mnt/gcs/$GCS_BUCKET_NAME /etc/letsencrypt/archive/$DOMAIN_NAME
 # fi
 
-#GOOGLE_APPLICATION_CREDENTIALS=/opt/etc/gcs.json /usr/sbin/gcsfuse $GCS_BUCKET_NAME /mnt/gcs/$GCS_BUCKET_NAME
+GOOGLE_APPLICATION_CREDENTIALS=/opt/etc/gcs.json /usr/sbin/gcsfuse $GCS_BUCKET_NAME $GCSMP
+
+if [ -e $GCSMP/letsencrypt/accounts ] ; then
+    cp -r $GCSMP/letsencrypt/accounts $LE_DIR/
+fi
+if [ -e $GCSMP/letsencrypt/csr ] ; then
+    cp -r $GCSMP/letsencrypt/csr $LE_DIR/
+fi
+if [ -e $GCSMP/letsencrypt/keys ] ; then
+    cp -r $GCSMP/letsencrypt/keys $LE_DIR/
+fi
+if [ -e $GCSMP/letsencrypt/renewal ] ; then
+    cp -r $GCSMP/letsencrypt/renewal $LE_DIR/
+fi
+if [ -e $GCSMP/letsencrypt/renewal ] ; then
+    cp -r $GCSMP/letsencrypt/renewal $LE_DIR/
+fi
+if [ -e $GCSMP/letsencrypt/archive ] ; then
+    cp -r $GCSMP/letsencrypt/archive $LE_DIR/
+    if [ ! -e $LE_DIR/live/$DOMAIN_NAME ] ; then
+        mkdir -p $LE_DIR/live/$DOMAIN_NAME
+    fi
+    if [ -d $LE_DIR/archive/$DOMAIN_NAME ] ; then
+        for ARC_NAME in "cert" "chain" "fullchain" "privkey"
+        do
+            ARCH_PATH=$(find $LE_DIR/archive/$DOMAIN_NAME/ \( -name $ARC_NAME\*.pem \) -print | tail -1)
+            ln -sfn ../../archive/$DOMAIN_NAME/$(basename $ARCH_PATH) $LE_DIR/live/$DOMAIN_NAME/$ARC_NAME.pem
+        done
+    fi
+fi
